7e96a49306bae3f30cb28648091bbf098888a433
SaeMatayoshi <c18016@std.it-college.ac.jp>
2019-08-07 03:52:57 +0900

    スクリプトの修正

--- GFF2019_Team4 Conveni/Assets/Scripts/Ito.cs
+++ GFF2019_Team4 Conveni/Assets/Scripts/Ito.cs
@@ -17,18 +17,6 @@ public class Ito : MonoBehaviour
 
     bool Iscount; 
 
-    // Use this for initialization
-    void Start()
-    {
-      
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // ３人目を助けようとしたときの蜘蛛出現の処理
